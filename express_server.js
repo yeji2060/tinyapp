92cfbfa1be78a0d3cb40494a4e2b00426504d49d
Yeji Ha <yeji206000@gmail.com>
2023-12-08 17:35:10 -0800

    modify header/server to display username with cookies

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -10,6 +11,7 @@ const urlDatabase = {
 };
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -28,7 +30,9 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase,
+  username: req.cookies["username"],
+ };
   res.render("urls_index", templateVars);
 });
 
@@ -82,6 +86,7 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 
 })
+
 
 function generateRandomString() {
   const alphanumericCharacters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
